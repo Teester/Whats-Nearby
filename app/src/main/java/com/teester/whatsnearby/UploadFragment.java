3d7f2a64cfcd32d3ff54eacb7c8ddadb7533a261
Mark Tully <markjtully@gmail.com>
2017-09-21 01:03:59 +0100

    Add a close button to the upload fragment

--- app/src/main/java/com/teester/whatsnearby/UploadFragment.java
+++ app/src/main/java/com/teester/whatsnearby/UploadFragment.java
@@ -1,9 +1,7 @@
 package com.teester.whatsnearby;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,18 +10,16 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-
 /**
  * Fragment to upload to osm
  */
 
-public class UploadFragment extends Fragment {
-	protected static final String ARG_PARAM1 = "param1";
+public class UploadFragment extends Fragment implements View.OnClickListener {
+
 	private static final String TAG = UploadFragment.class.getSimpleName();
-	public boolean logged_in = false;
+
 	private OnFragmentInteractionListener mListener;
-	private ArrayList<Answer> answers;
+
 	private TextView thanks_textview;
 	private ImageView thanks_imageView;
 	private Button authorise;
@@ -33,20 +29,7 @@ public class UploadFragment extends Fragment {
 	}
 
 	public static UploadFragment newInstance() {
-		ArrayList<Answer> answers = new ArrayList<Answer>();
-		UploadFragment fragment = new UploadFragment();
-		Bundle args = new Bundle();
-		args.putParcelableArrayList(ARG_PARAM1, answers);
-		fragment.setArguments(args);
-		return fragment;
-	}
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		if (getArguments() != null) {
-			this.answers = QuestionsActivity.answers;
-		}
+		return new UploadFragment();
 	}
 
 	@Override
@@ -56,25 +39,10 @@ public class UploadFragment extends Fragment {
 		this.thanks_textview = questionFragmentView.findViewById(R.id.thanks_textview);
 		this.thanks_imageView = questionFragmentView.findViewById(R.id.thanks_imageView);
 		this.authorise = questionFragmentView.findViewById(R.id.authorise);
+
+		this.authorise.setOnClickListener(this);
+
 		return questionFragmentView;
-	}
-
-	@Override
-	public void onStart() {
-		super.onStart();
-
-		SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
-		this.logged_in = sharedPreferences.getBoolean("logged_in_to_osm", false);
-
-		//this.answers = QuestionsActivity.answers;
-		if (this.logged_in = true) {
-			this.thanks_imageView.setImageResource(R.drawable.ic_yes);
-			this.thanks_textview.setText(R.string.upload_thanks);
-			this.authorise.setVisibility(View.INVISIBLE);
-		} else {
-			OAuth oAuth = new OAuth(this.getContext());
-			oAuth.execute();
-		}
 	}
 
 	@Override
@@ -92,6 +60,11 @@ public class UploadFragment extends Fragment {
 	public void onDetach() {
 		super.onDetach();
 		mListener = null;
+	}
+
+	@Override
+	public void onClick(View view) {
+		getActivity().finish();
 	}
 
 	public interface OnFragmentInteractionListener {
