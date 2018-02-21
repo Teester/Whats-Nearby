e3aa5c2f00e9dcb4c7b54a6165c4aa54a2ef2a36
Mark Tully <markjtully@gmail.com>
2018-02-21 01:01:01 +0000

    Fix tests & testing environment

--- app/src/androidTest/java/com/teester/whatsnearby/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/teester/whatsnearby/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
 		// Context of the app under test.
 		Context appContext = InstrumentationRegistry.getTargetContext();
 
-		assertEquals("com.teester.mapquestions", appContext.getPackageName());
+		assertEquals("com.teester.whatsnearby", appContext.getPackageName());
 	}
 }
