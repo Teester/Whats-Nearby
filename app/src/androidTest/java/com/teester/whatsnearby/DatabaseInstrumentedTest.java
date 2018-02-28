c2b1120794bda066207d4fc0fbf04fa816c251fd
Mark Tully <markjtully@gmail.com>
2018-02-28 01:13:13 +0000

    Adding instrumented tests from Room database

---
+++ app/src/androidTest/java/com/teester/whatsnearby/DatabaseInstrumentedTest.java
@@ -0,0 +1,95 @@
+package com.teester.whatsnearby;
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule;
+import android.arch.persistence.room.Room;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.teester.whatsnearby.data.OsmObject;
+import com.teester.whatsnearby.data.localDatabase.AppDatabase;
+import com.teester.whatsnearby.data.localDatabase.VisitedLocation;
+import com.teester.whatsnearby.data.localDatabase.VisitedLocationDao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+public class DatabaseInstrumentedTest {
+
+	@Rule
+	public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+
+	private AppDatabase mDatabase;
+	private VisitedLocationDao visitedLocationDao;
+	private OsmObject osmObject = new OsmObject(1, "", "1", "", 1, 1, 1);
+	private OsmObject osmObject2 = new OsmObject(2, "", "2", "", 1, 1, 1);
+	private OsmObject osmObject3 = new OsmObject(3, "", "3", "", 1, 1, 1);
+	private OsmObject osmObject4 = new OsmObject(4, "", "4", "", 1, 1, 1);
+	private OsmObject osmObject5 = new OsmObject(5, "", "5", "", 1, 1, 1);
+
+	@Before
+	public void initDb() throws Exception {
+		mDatabase = Room.inMemoryDatabaseBuilder(InstrumentationRegistry.getContext(),
+				AppDatabase.class)
+				.allowMainThreadQueries()
+				.build();
+
+		visitedLocationDao = mDatabase.visitedLocationDao();
+	}
+
+	@After
+	public void closeDb() throws Exception {
+		mDatabase.close();
+	}
+
+	@Test
+	public void onFetchingLocations_shouldGetEmptyList_IfTable_IsEmpty() throws InterruptedException {
+		List<VisitedLocation> LocationList = visitedLocationDao.getAllVisitedLocations();
+		assertTrue(LocationList.isEmpty());
+	}
+
+	@Test
+	public void onInsertingLocations_checkIf_RowCountIsCorrect() throws InterruptedException {
+		visitedLocationDao.insert(new VisitedLocation(osmObject));
+		visitedLocationDao.insert(new VisitedLocation(osmObject2));
+		visitedLocationDao.insert(new VisitedLocation(osmObject3));
+		visitedLocationDao.insert(new VisitedLocation(osmObject4));
+		visitedLocationDao.insert(new VisitedLocation(osmObject5));
+
+		assertEquals(5, visitedLocationDao.getAllVisitedLocations().size());
+	}
+
+	@Test
+	public void onUpdatingALocation_checkIf_UpdateHappensCorrectly() throws InterruptedException {
+		VisitedLocation visitedLocation = new VisitedLocation(osmObject);
+		visitedLocationDao.insert(visitedLocation);
+
+		visitedLocation = visitedLocationDao.findByOsmId(osmObject.getId());
+		visitedLocation.setName("New Name");
+		visitedLocationDao.update(visitedLocation);
+
+		assertEquals(1, visitedLocationDao.getAllVisitedLocations().size());
+		assertEquals("New Name", visitedLocationDao.findByOsmId(osmObject.getId()).getName());
+	}
+
+	@Test
+	public void onLocationDeletion_CheckIf_LocationIsDeletedFromTable() {
+		visitedLocationDao.insert(new VisitedLocation(osmObject));
+		visitedLocationDao.insert(new VisitedLocation(osmObject2));
+		visitedLocationDao.insert(new VisitedLocation(osmObject3));
+		visitedLocationDao.insert(new VisitedLocation(osmObject4));
+		visitedLocationDao.insert(new VisitedLocation(osmObject5));
+
+		visitedLocationDao.delete(visitedLocationDao.findByOsmId(osmObject2.getId()));
+		assertNull(visitedLocationDao.findByOsmId(osmObject2.getId()));
+	}
+}
